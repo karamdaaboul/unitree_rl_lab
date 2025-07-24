78ecc41fb6c820ad4c5f7d7d2a0e865d5234ed79
Karam Daaboul <m.k.daaboul@gmail.com>
2025-07-24 15:21:42 +0200

    add the pedipulation task

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/go2/__init__.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/go2/__init__.py
@@ -19,3 +19,23 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
     },
 )
+
+gym.register(
+    id="Unitree-Go2-Pedipulation",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.pedipulation_env_cfg:Go2PedipulationEnvCfg",
+        "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Unitree-Go2-Pedipulation-Play",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.pedipulation_env_cfg:Go2PedipulationPlayEnvCfg",
+        "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
+    },
+)
