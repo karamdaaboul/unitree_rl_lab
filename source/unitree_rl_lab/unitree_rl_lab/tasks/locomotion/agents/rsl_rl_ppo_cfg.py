c4a353acd989db752c777c758ffed82cd9b5c9fe
Karam Daaboul <mohammd.daaboul@kit.edu>
2025-08-19 10:47:49 +0200

    add new config

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/agents/rsl_rl_ppo_cfg.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/agents/rsl_rl_ppo_cfg.py
@@ -7,8 +7,9 @@ from typing import Union
 from dataclasses import MISSING
 
 from isaaclab.utils import configclass
-from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg
-from unitree_rl.safe_rl.algorithm_cfg import RslRlP3OAlgorithmCfg
+from unitree_rl.safe_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoActorCriticRecurrentCfg, RslRlPpoAlgorithmCfg
+from unitree_rl.safe_rl import RslRlP3oActorCriticCfg, RslRlP3oAlgorithmCfg
+
 
 @configclass
 class BasePPORunnerCfg(RslRlOnPolicyRunnerCfg):
@@ -39,10 +40,24 @@ class BasePPORunnerCfg(RslRlOnPolicyRunnerCfg):
     )
 
 @configclass
+class RecurrentPPORunnerCfg(BasePPORunnerCfg):
+    policy = RslRlPpoActorCriticRecurrentCfg(
+        class_name="ActorCriticRecurrent",
+        init_noise_std=1.0,
+        actor_hidden_dims=[200, 100],
+        critic_hidden_dims=[200, 100],
+        activation="elu",
+        rnn_type="lstm",
+        rnn_hidden_dim=128,
+        rnn_num_layers=1,
+    )
+
+
+@configclass
 class BaseP3ORunnerCfg(BasePPORunnerCfg):
     """Configuration for P3O (Penalized PPO) runner."""
     
-    algorithm = RslRlP3OAlgorithmCfg(
+    algorithm = RslRlP3oAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
@@ -59,7 +74,6 @@ class BaseP3ORunnerCfg(BasePPORunnerCfg):
         #kappa_init=1.0,  # Initial penalty factor
         kappa_max=100.0,  # Maximum penalty factor
         rho=1.5,  # Penalty factor multiplier (ρ > 1)
-        cost_thresholds=[0.1],  # Cost threshold for constraints
         adaptive_penalty=True,  # Whether to use adaptive penalty updates
         constraint_margin=0.85,  # Constraint violation detection margin
         use_clipped_cost_loss=True,  # Whether to use clipped cost value loss
